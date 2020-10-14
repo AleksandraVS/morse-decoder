23e5ca9fffbff884f7fba1b83e83460d43de7dbe
Sasha Vrublevskaya <Belarus>
2020-10-15 00:00:55 +0300

    .

--- src/index.js
+++ src/index.js
@@ -38,8 +38,52 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let reg = /[0,1,*]{10}/g;
+    let arr = expr.match(reg);
+    let temp = "";
+  
+   for (let i = 0; i < arr.length; i++)
+  {
+    if (arr[i] !== "**********") { 
+      arr[i] = arr[i].substring(arr[i].indexOf("1"));
+    }
+    else arr[i] = " ";
+  }
+   reg = /[10,11]{2}/g;
+  
+   for (let i = 0; i < arr.length; i++) 
+  { 
+    let tempArr = [];
+     if (arr[i] !== " ") 
+   {
+    tempArr = arr[i].match(reg);
+    for (let j = 0; j < tempArr.length; j++) 
+   {
+    if (tempArr[j] == "10") 
+    tempArr[j] = ".";
+    else tempArr[j] = "-";
+   }
+  
+   for (let j = 0; j < tempArr.length; j++)
+   temp += tempArr[j];
+  
+   arr[i] = temp;
+   temp = "";
+  
+  }
+  
+  for (let i = 0; i < arr.length; i++) {
+  for (let code in MORSE_TABLE) {
+    if (arr[i] === code)
+    arr[i] = MORSE_TABLE[code];
+  }
+  }
+  }
+  for (let i = 0; i < arr.length; i++)
+  temp += arr[i]; 
+  return temp;
+  }
+  
 
 module.exports = {
     decode
